829eb76c0ed8dafcb899337023d6b7018405bda1
Jonny <jonny.hong91@gmail.com>
2017-08-26 18:38:31 +0800

    板1

--- Board1/Board1.h
+++ Board1/Board1.h
@@ -1,3 +1,10 @@
-code int TICKER = 5*60*1000/50; //50毫秒几次
+code int TICKER = 60*1000*5;
 
+code int gameBAnswer[4] = {3,1,2,2};
 code int CORRECT[10] = {1,2,1,2,2,1,2,2,1,1};
+
+code int MUSIC_301 = 0x01; //打开2号房间门
+code int MUSIC_302 = 0x02;//A完成
+code int MUSIC_303 = 0x03;//激光介绍
+code int MUSIC_304 = 0x04;//B完成
+code int MUSIC_305 = 0x05;//C完成
--- Board1/Main.c
+++ Board1/Main.c
@@ -4,8 +4,21 @@
 #include "uart.h"
 #include "mp3.h"
 
+typedef unsigned char BYTE;
+typedef unsigned int WORD;
+#define FOSC 11059200L
+#define MODE1T                      //Timer clock mode, comment this line is 12T mode, uncomment is 1T mode
+
+#ifdef MODE1T
+#define T1MS (65536-FOSC/1000)      //1ms timer calculation method in 1T mode
+#else
+#define T1MS (65536-FOSC/12/1000)   //1ms timer calculation method in 12T mode
+#endif
+
 //口令输入
 sbit FROM_ROOM1 = P0^0;
+sbit ROOM_2 = P2^7;
+
 sbit LIGHT_1 = P0^1;
 sbit LIGHT_2 = P0^2;
 sbit LIGHT_3 = P0^3;
@@ -16,7 +29,10 @@ sbit LIGHT_INPUT_2 = P1^2;
 sbit LIGHT_INPUT_3 = P1^3;
 sbit LIGHT_INPUT_4 = P1^4;
 
+int isIntroduceLight = 0;//是不是介绍过了激光
 
+sbit INPUT_A = P3^7;
+sbit GAME_A_LOCK = P3^2;
 
 sbit TO_BOARD_2 = P0^6;
 
@@ -32,8 +48,6 @@ sbit ROOM_3_DOOR = P3^6;//房间3的门
 
 int i = 0;
 int five_minute_light_step = 0;
-int myAnswer[10] = {0,0,0,0,0,0,0,0,0,0};
-int answerLength = 10;
 
 void start();
 void timer0();
@@ -45,6 +59,8 @@ void gameC();
 void init()
 {
 	FROM_ROOM1 = 0;
+	ROOM_2 = 1;
+	
 	LIGHT_1 = 1;
 	LIGHT_2 = 1;
 	LIGHT_3 = 1;
@@ -54,6 +70,9 @@ void init()
 	LIGHT_INPUT_3 = 1;
 	LIGHT_INPUT_4 = 1;
 	ABC_SWITCH = 1;
+	
+	INPUT_A = 0;
+	GAME_A_LOCK = 1;
 	
 	GAME_B_BTN1 = 0;
 	GAME_B_BTN2 = 0;
@@ -93,9 +112,11 @@ void main()
 
 void start()
 {
-	if(FROM_ROOM1 == 1)
+	if(FROM_ROOM1 == 1) //口令正确 打开门
 	{
 		delay_ms(2000);
+		ROOM_2 = 0;
+		play_mp3(0,MUSIC_301);
 		setStep(1);
 	}
 }
@@ -108,6 +129,12 @@ void checkLightStep()
 		LIGHT_2 = 0;
 		LIGHT_3 = 0;
 		LIGHT_4 = 0;
+		
+		if(isIntroduceLight == 0)
+		{
+			play_mp3(0,MUSIC_303); // 第一次触发介绍激光
+			isIntroduceLight = 1;
+		}
 	}
 	else if(five_minute_light_step == 2)
 	{
@@ -205,56 +232,77 @@ void fiveMinutes()
 
 void gameA()
 {
-	
+	if(INPUT_A == 1)
+	{
+		delay_ms(50);
+		if(INPUT_A == 1)
+		{
+			play_mp3(0,MUSIC_302);
+			i = TICKER;	//计时器归0
+			GAME_A_LOCK = 0;//打开电磁锁
+			gameStep = 2;
+		}
+	}
 }
 
-
+int isGameBButton1Pressed = 0;
+int isGameBButton2Pressed = 0;
+int gameBMyAnswer[4] = {0,0,0,0};
 void gameB()
 {
 	int btn = 0;
-	if(GAME_B_BTN1 == 0)
+		
+	if((isGameBButton1Pressed == 0)&&(GAME_B_BTN1 == 1))
 	{
-		delay_ms(20);
-		if(GAME_B_BTN1 == 1)
-		{
-			btn = 1;
-		}
+		isGameBButton1Pressed = 1;
 	}
 	
-	if(GAME_B_BTN2 == 0)
+	if((isGameBButton2Pressed == 0)&&(GAME_B_BTN2 == 1))
 	{
-		delay_ms(20);
-		if(GAME_B_BTN2 == 1)
-		{
-			btn = 2;
-		}
+		isGameBButton2Pressed = 1;
 	}
 	
+	if((isGameBButton1Pressed == 1)&&(isGameBButton2Pressed == 1))//同时按下
+	{
+		while((isGameBButton1Pressed == 1)&&(isGameBButton2Pressed == 1)){}; //直到松开
+		isGameBButton1Pressed = 0;
+		isGameBButton2Pressed = 0;
+		
+		btn = 3;
+	}
+	
+	if((isGameBButton1Pressed == 1)&&(isGameBButton2Pressed == 0))
+	{
+		while((isGameBButton1Pressed == 1)&&(isGameBButton2Pressed == 0));
+		isGameBButton1Pressed = 0;
+		isGameBButton2Pressed = 0;
+		btn = 1;
+	}
+	
+	if((isGameBButton1Pressed == 0)&&(isGameBButton2Pressed == 1))
+	{
+		while((isGameBButton1Pressed == 0)&&(isGameBButton2Pressed == 1));
+		isGameBButton1Pressed = 0;
+		isGameBButton2Pressed = 0;
+		btn = 2;
+	}
+
 	if(btn != 0)
 	{
-		int answerIndex = 0;
-		for(answerIndex = 0 ; answerIndex < answerLength-1 ; answerIndex++ )
+		gameBMyAnswer[0] = gameBMyAnswer[1];
+		gameBMyAnswer[1] = gameBMyAnswer[2];
+		gameBMyAnswer[2] = gameBMyAnswer[3];
+		gameBMyAnswer[3] = btn;
+
+		if((gameBMyAnswer[0] == gameBAnswer[0])&&
+		(gameBMyAnswer[1] == gameBAnswer[1])&&
+		(gameBMyAnswer[2] == gameBAnswer[2])&&
+		(gameBMyAnswer[3] == gameBAnswer[3]))//正确
 		{
-			myAnswer[answerIndex] = myAnswer[answerIndex+1];
-		}
-		myAnswer[answerLength - 1] = btn;
-		
-		for(answerIndex == 0 ; answerIndex<answerLength ; answerIndex++)
-		{
-			if(CORRECT[answerIndex] == myAnswer[answerIndex])
-			{
-				if(answerIndex == answerLength - 1)
-				{
-					i = TICKER;	//计时器归0
-					GAME_B_LOCK = 0;//电磁锁断电
-					gameStep = 3;
-					return;
-				}
-				else
-				{
-					return;
-				}
-			}
+			play_mp3(0,MUSIC_304);
+			i = TICKER;	//计时器归0
+			GAME_B_LOCK = 0;//电磁锁断电
+			gameStep = 3;
 		}
 	}
 }
@@ -263,6 +311,7 @@ void gameC()
 {
 	if((GAME_C_1 == 1)&&(GAME_C_2 == 1))
 	{
+		play_mp3(0,MUSIC_305);
 		i = TICKER;	//计时器归0
 		ET0 = 0;//计时器停止
 		gameStep = 4;
@@ -277,8 +326,8 @@ void gameC()
 
 void Timer_Routine(void) interrupt 1
 {
-	TL0 = 0x00;
-	TH0 = 0x4C;
+	TL0 = T1MS;                     //reload timer0 low byte
+    TH0 = T1MS >> 8;                //reload timer0 high byte
 
     i--;
 
@@ -295,12 +344,14 @@ void Timer_Routine(void) interrupt 1
 void timer0()
 {
     i = TICKER;	
-
-		TMOD = 0x01;
-		TL0 = 0x00;	
-		TH0 = 0x4C;
-		ET0 = 1;
-    EA  = 1;
-    TR0 = 1;
-
+	
+#ifdef MODE1T
+    AUXR = 0x80;                    //timer0 work in 1T mode
+#endif
+    TMOD |= 0x01;                    //set timer0 as mode1 (16-bit)
+    TL0 = T1MS;                     //initial timer0 low byte
+    TH0 = T1MS >> 8;                //initial timer0 high byte
+    TR0 = 1;                        //timer0 start running
+    ET0 = 1;                        //enable timer0 interrupt
+    EA = 1;                         //open global interrupt switch
 }
